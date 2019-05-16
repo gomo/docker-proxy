f90e4771dcd1b3ccc69f2d5386d7e6f1cf55c345
Masamoto Miyata <miyata@sincere-co.com>
2019-05-16 16:35:55 +0900

    コンテナリストを実装。

---
+++ index/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const { fetchDockerContainers } = require('../lib/util')
+const IntlRelativeFormat = require('intl-relativeformat')
+const format = require('date-fns/format')
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '/views'));
+
+console.log(fetchDockerContainers())
+
+const relativeFormat = new IntlRelativeFormat('ja')
+
+app.use((req, res, next) => {
+  res.render('index.ejs', {
+    containers: fetchDockerContainers(),
+    relativeFormat: relativeFormat,
+    format: format
+  })
+})
+
+app.listen(8080)
