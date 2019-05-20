2dea1a4d53f0183ae6ed0feeb573847ca0256931
Masamoto Miyata <miyata@sincere-co.com>
2019-05-20 14:59:39 +0900

    gitリポジトリに関する情報を表示。

--- index/app.js
+++ index/app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { fetchDockerContainers } = require('../lib/util')
+const { fetchDockerContainers, fetchDockerContainerInfo } = require('../lib/util')
 const IntlRelativeFormat = require('intl-relativeformat')
 const format = require('date-fns/format')
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, '/views'));
+app.use('/node-static', express.static(path.join(__dirname, '/public')))
 
 const relativeFormat = new IntlRelativeFormat('ja')
+
 
 app.use((req, res, next) => {
   res.render('index.ejs', {
     containers: fetchDockerContainers(),
     relativeFormat: relativeFormat,
-    format: format
+    format: format,
+    fetchDockerContainerInfo: fetchDockerContainerInfo
   })
 })
 
