346e5745be7ad0d0524aaa3712c9b47f8eb8d698
Masamoto Miyata <miyata@sincere-co.com>
2019-05-21 14:16:50 +0900

    MVC ログを出してみた。

--- index/app.js
+++ index/app.js
@@ -1,38 +1,15 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const { fetchDockerContainers, fetchDockerContainerInfo } = require('../lib/util')
-const IntlRelativeFormat = require('intl-relativeformat')
-const format = require('date-fns/format')
 
+const app = express();
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, '/views'));
-app.use('/node-static', express.static(path.join(__dirname, '/public')))
 
-const relativeFormat = new IntlRelativeFormat('ja')
+const log = require('../lib/log')
+const index = require('./routes/index');
 
-
-app.use((req, res, next) => {
-  const containers = fetchDockerContainers()
-  const using_ports = containers.map(c => parseInt(c.http_port, 10))
-  const avaiable_ports = []
-  let port = Math.min.apply(null, using_ports)
-
-  while(true){
-    ++port//Math.minで取ってるので最初のは必ず使用中
-    if(!using_ports.includes(port)){
-      avaiable_ports.push(port)
-      if(avaiable_ports.length >= 1) break;
-    }
-  }
-
-  res.render('index.ejs', {
-    containers: containers,
-    relativeFormat: relativeFormat,
-    format: format,
-    fetchDockerContainerInfo: fetchDockerContainerInfo,
-    avaiable_ports: avaiable_ports
-  })
-})
+app.use(log)
+app.use('/docker-proxy/static', express.static(path.join(__dirname, '/public')))
+app.use(index)
 
 app.listen(8080)
---
+++ index/routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const { fetchDockerContainers, fetchDockerContainerInfo } = require('../../lib/util')
+const IntlRelativeFormat = require('intl-relativeformat')
+const format = require('date-fns/format')
+
+const relativeFormat = new IntlRelativeFormat('ja')
+
+//getの例
+router.get(/.*/, (req, res, next) => {
+  const containers = fetchDockerContainers()
+  const using_ports = containers.map(c => parseInt(c.http_port, 10))
+  const avaiable_ports = []
+  let port = Math.min.apply(null, using_ports)
+
+  while(true){
+    ++port//Math.minで取ってるので最初のは必ず使用中
+    if(!using_ports.includes(port)){
+      avaiable_ports.push(port)
+      if(avaiable_ports.length >= 1) break;
+    }
+  }
+
+  res.render('index.ejs', {
+    containers: containers,
+    relativeFormat: relativeFormat,
+    format: format,
+    fetchDockerContainerInfo: fetchDockerContainerInfo,
+    avaiable_ports: avaiable_ports
+  })
+});
+
+module.exports = router
