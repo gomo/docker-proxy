7bf8db8484bc42445411d66744414623f3c59bd0
Masamoto Miyata <miyata@sincere-co.com>
2019-05-22 19:15:54 +0900

    ファビコン

--- index/app.js
+++ index/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const fs = require("fs")
 
 const app = express();
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, '/views'));
 
-// app.use(require('../lib/log'))
+app.use(require('../lib/log'))
 app.use('/docker-proxy/static', express.static(path.join(__dirname, '/public')))
 
 const pages = [
@@ -13,6 +14,11 @@ const pages = [
 ]
 
 pages.forEach(page => app.use(`/docker-proxy/${page}`, require(`./routes/${page}`)))
+app.use('/favicon.ico', (req, res, next) => {
+  var buf = fs.readFileSync(path.join(__dirname, '/public/favicon.ico'));
+  res.writeHead(200, {"Content-Type": "image/vnd.microsoft.icon"});
+  res.end(buf);
+})
 app.use(require('./routes/index'))
 
 app.listen(8080)
